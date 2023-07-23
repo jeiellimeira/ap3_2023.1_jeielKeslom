833b793f3fb501a3871c78adab6ca76757de8f34
Jeiel limeira <139088969+jeiellimeira@users.noreply.github.com>
2023-07-23 13:14:26 -0300

    Update OlaMundo.java
    só mudei fiz alguns testes para entender melho como funciona

--- unidade01/src/OlaMundo.java
+++ unidade01/src/OlaMundo.java
@@ -1,6 +1,19 @@
 public class OlaMundo {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        
-    }
+    public static void main(String[] args){
+
+        System.out.println(2+2);
+        System.out.println(2+6);
+        System.out.println("ola mundo");System.out.println(10/3);
+
+        //comentários são utéis para indicar comentários/notas no projeto
+
+        // ponto de entrada - entry point -> é o ponto onde todos sistemas procuram no seu projeto para começa o projeto.
+
+        // começar a iniciar o projeto 
+
+        //metodos: sao instruçoes que define o programar executar - exemplo:
+
+        //instrução chamada system.out.println("")
+        // propriedades/variaveis -> sera onde iremos armazenar os dados para manipula-los no futuro
+        }
 }
