4e9b2ac8d964a36377b6671858930b1df2f94ac2
Jeiel limeira <139088969+jeiellimeira@users.noreply.github.com>
2023-08-19 15:47:17 -0300

    correção conta bancaria
    :)

--- unidade01/src/contaBancaria.java
+++ unidade01/src/contaBancaria.java
@@ -1,38 +1,123 @@
-//Crie uma classe chamada "ContaBancaria" que tenha os atributos número da conta, nome do titular e saldo. Implemente métodos para depositar, sacar e verificar o saldo da conta. Implemente getters, setters e toString. Crie a classe TestaContaBancaria, com 3 contas.
- // 10104235940
-public class contaBancaria {
-    String NomeConta;
-    float saldo;
-    String NumeroConta;
-    float dep = 500f;
+
+public class ContaBancaria {
+   //Atributos
+    public int numConta;
+    protected String tipo;
+    private String dono;
+    private float saldo;
+    private boolean status;
+
+    public void estadoAtual(){
+    System.out.println("-----------------------------");
+    System.out.println("conta: " + this.getNumConta());
+    System.out.println("tipo: " +this.getTipo());
+    System.out.println("dono: "+ this.getDono());
+    System.out.println("saldo:" + this.getSaldo());
+    }
     
-
-    public contaBancaria(String nc, String nmc, float s,float d) {
-        this.NumeroConta = nc;
-        this.NomeConta = nmc;
-        this.saldo = s;
-        this.dep = d;
-    }
-
-    public void status(){
-        System.out.println("Nome do usuario: " +this.NomeConta);
-        System.out.println("Número da conta: "+ this.NumeroConta);
-        System.out.println("Saldo: R$"+this.saldo);
-    }
-    public void verificar(){
-        System.out.println("o seu saldo atual é de: R$" +this.saldo );
-    }
-
-    public void sacar(){
-        if (saldo > 0) {
-            System.out.println("voce pode sacar");
-        } else {
-            System.out.println("ERRO voce não pode sacar");
+    //metodos personalizados
+    
+    public void abrirConta(String t){
+        this.setTipo(t);
+        this.setStatus(true);
+        if (t == "CC"){
+            this.setSaldo(50);
+        }else if  (t =="CP") {
+            this.setSaldo(150);
         }
+        System.out.println("conta aberta com sucesso");
+            
+        
+    }
+    public void fecharConta(){
+        if (this.getSaldo()>0){
+            System.out.println("Conta não pode ser fechada porque ainda tem dinheiro");
+        } else if(this.getSaldo()<0){
+            System.out.println("conta não pode ser fechada pois tem débito");
+        }else{
+           this.setStatus(false);
+            System.out.println("Conta fechada com sucesso");
+            }
+        }
+            
+    public void depositar(float v){
+        if (this.getStatus()){
+           // this.saldo = this.saldo + v;
+            this.setSaldo(this.getSaldo() + v);
+            System.out.println("Depósito realizado na conta de: " + this.getDono());
+        }else{
+            System.out.println("impossivel depositar em uma conta fechada!");
+        }
+
+        }
+
+    public void sacar(float v){
+        
+        if (this.getStatus()){
+            if (this.getSaldo() >= v ) {
+                this.setSaldo(this.getSaldo() - v);
+                System.out.println("Saque realizado na conta de: "+getDono());
+            }else{
+                System.out.println("Saldo insuficiente para saque");
+            }
+        }else{
+            System.out.println("impossivel sacar uma conta fechada!");
+        }
+                
+            }
+
+    public void pagarMensal(){
+        int v = 0;
+        if (this.getTipo() == "CC"){
+            v=12;
+        } else if (this.getTipo() =="CP"){
+            v=20;
+        }
+        if (this.getStatus()){
+        this.setSaldo(this.getSaldo() - v);
+        System.out.println("mensalidade paga com sucesso por "+ this.getDono());
+        }else{
+            System.out.println("impossivel pagar com uma conta fechada!");
+        }
+        
     }
 
-    public void depositar(){
-         System.out.println("você depositou: R$"+this.dep);
+    //métodos especiais
+
+    public void ContaBanco(){
+        this.saldo = 0;
+        this.status = false;
     }
+    public int getNumConta() {
+        return numConta;
+    }
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String t) {
+        this.tipo = t;
+    }
+    public String getDono() {
+        return dono;
+    }
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+    public float getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+    public boolean getStatus() {
+        return status;
+    }
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
     
 }
--- unidade01/src/testaContaBancaria.java
+++ unidade01/src/testaContaBancaria.java
@@ -1,30 +1,35 @@
-class TestaContaBancaria {
-    public static void main(String[] args) {
-        contaBancaria cb1 = new contaBancaria ("1726125126", "Goku", 8001f,500);
-        // 10104235940
-        cb1.status();
-        cb1.verificar();
-        cb1.sacar();
-        cb1.depositar();
-        
-        
-        System.out.println();
-        
-        contaBancaria cb2 = new contaBancaria ("1022123126", "gohan", 82001f,90);
-        cb2.status();
-        cb2.verificar();
-        cb2.sacar();
-        cb2.depositar();
 
-        
-        System.out.println();
-        
-        contaBancaria cb3 = new contaBancaria ("1332332336", "picollo", -1f,0);
-       
-        cb2.verificar();
-        cb2.sacar();
-        cb2.depositar();
+public class TestaContaBancaria {
+public static void main(String[] args) {
+    ContaBancaria p1 = new ContaBancaria();
+    
+    p1.setNumConta(1111);
+    p1.setDono("gohan");
+    p1.abrirConta("CC");
+    p1.depositar(100);
 
-        
-    }
+ContaBancaria p2 = new ContaBancaria();
+    p2.setNumConta(2222);
+    p2.setDono("goku");
+    p2.abrirConta("CP");
+    p2.depositar(500);
+ContaBancaria p3 = new ContaBancaria();
+p3.setNumConta(3333);
+p3.setDono("kuririn");
+p3.abrirConta("CP");
+p3.depositar(500);
+p3.sacar(100);
+
+    
+
+    
+
+
+    
+    p1.estadoAtual();
+    p2.estadoAtual();
+    p3.estadoAtual();
+    
+}
+    
 }
